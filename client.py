5ff0b52bec3c027834f2afe7a65ae50215f26786
chrisabos <cbos95@csu.fullerton.edu>
2019-05-16 18:21:12 -0700

    created multi threaded client/server

--- client.py
+++ client.py
@@ -1,20 +1,47 @@
 import socket
-s = socket.socket()
-host = socket.gethostname()
-port = 8080
-s.connect((host,port))
-print("Connected to the server")
+from _thread import *
 
-# connection been connected message
-message = s.recv(1024)
-message = message.decode()
-print(message)
+#initialize client socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ipaddr = '127.0.0.1'
+port = 64646
 
-while True:
-    message = s.recv(1024)
-    message = message.decode()
-    print(message)
-    
-    new_message = input(str(">> "))
-    new_message = new_message.encode()
-    s.send(new_message)
+#connect to the server
+sock.connect((ipaddr, port))
+
+#thread_socket is a thread that will handled all traffic from the socket
+def thread_socket():
+
+	#thread main loop
+	while True:
+		try:
+			#try to recieve a message from the server
+			msg = sock.recv(1024)
+
+			#if a message has been recieved
+			if msg:
+				#print the message
+				print(msg.decode())
+
+		except:
+			continue
+
+#variabled
+running = True
+
+while running:
+
+	#initialize the recieving loop
+	start_new_thread(thread_socket, ())
+
+	#main loop for listening to client inputs
+	while True:
+
+		#get the input from the user
+		msg = input()
+
+		#send the message to the server
+		if msg:
+			sock.send(msg.encode())
+
+sock.close()
--- server.py
+++ server.py
@@ -1,30 +1,90 @@
 import socket
+from _thread import *
 
-s = socket.socket()
-host = socket.gethostname()
-port = 8080
-s.bind((host,port))
-s.listen(1)
-print("Starting up CPSC 452 Chat Server...")
-print("Waiting for 2 connections...")
-client_one, client_one_addr = s.accept()
-print("Client one has been connected...")
-client_one.send("Welcome to CPSC 452 Chat Server (Client One)".encode())
-print("Waiting for 1 connection...")
-client_two, client_two_addr = s.accept()
-print("Client two has been connected...")
-client_two.send("Welcome to CPSC 452 Chat Server (Client 2)".encode())
+#set up server socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-message = "process...".encode()
-client_one.send(message)
+ipaddr = '127.0.0.1'
+port = 64646
 
-while True:
-        recv_message = client_one.recv(1024)
-        message = "Client One: " + recv_message.decode()
-        print("Client One message: ", recv_message.decode())
-        client_two.send(message.encode())
-        
-        recv_message = client_two.recv(1024)
-        message = "Client Two: " + recv_message.decode()
-        print("Client Two message: ", recv_message.decode())
-        client_one.send(message.encode())
+server.bind((ipaddr, port))
+server.listen(100)
+
+#this list will contain all active clients
+clientList = []
+
+#thread_client will be a thread that is running for each client to handled messages
+def thread_client(conn, addr):
+
+	#welcome the client
+	welcomeMsg = "Welcome to Secure Chat!"
+	conn.send(welcomeMsg.encode())
+
+	#client main loop
+	while True:
+		try:
+			#try to recieve a message from the client
+			msg = conn.recv(1024)
+
+			#if a message has been recieved
+			if msg:
+				#print the message for debugging
+				#TODO: remove this print to because eventually all the traffic will be encrypted
+				print(addr[0] + ": " + msg.decode())
+
+				#send the message to all the connected users
+				broadcast(msg, conn)
+			else:
+				#this will handled when the server recieves a weird message. either a disconnect or error
+				#regardless we want to disconnect the client
+				remove(conn)
+
+		except:
+			continue
+
+#broadcast will send a message to all currently connected users except for the one passed as 'conn'
+#pass conn=null to broadcast to all
+def broadcast(msg, conn):
+	#loop through all currently connected clients and send the message
+	for client in clientList:
+
+		#if the client is not the one specified to be ignored
+		if client != conn:
+			try:
+				#send the message to the client
+				client.send(msg)
+			except:
+				#if something goes wrong then disconnect the client
+				client.close()
+				remove(client)
+
+#remove will remove a client from the client list
+def remove(conn):
+	if conn in clientList:
+		clientList.remove(conn)
+
+#the main function of the program
+def main():
+	print('Starting Server')
+
+	print('Waiting for clients...')
+	#our main server loop
+	while True:
+
+		#accept a client
+		conn, addr = server.accept()
+
+		#add the connection onto the client list in order to make sure the list is always accurate
+		clientList.append(conn)
+
+		print(addr[0] + ": connected!", addr)
+
+		#create a new thread for the newly connected client to handled all things related to that clientList
+		start_new_thread(thread_client, (conn, addr))
+
+
+#start the server
+main()
+conn.close()
+server.close()
