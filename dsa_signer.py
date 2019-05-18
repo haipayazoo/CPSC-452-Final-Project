872c816d3af0d1691b399b505ea50bf510424881
Cameron Mathos <haipayazoo@gmail.com>
2019-05-18 13:36:43 -0700

    DSA so far

--- dsa_signer.py
+++ dsa_signer.py
@@ -1,8 +1,24 @@
 from Crypto.PublicKey import DSA
 from Crypto.Hash import SHA512
-from Crypto.Random import random
 
-def sign():
+# Sign the contents of the message
+def sign(message, key):
+    hash = SHA512.new(message).hexdigest()
 
-def verify():
+    signature = key.sign(hash, key)
+
+    signed_message = message + str(signature[0])
+
+    return signed_message
+
     
+
+# Verify the authenticity of the message
+#def verify():
+    
+
+message = "Baboons rule! xDDDD"
+key = DSA.generate(1024)
+message = sign(message, key)
+
+print(message)
