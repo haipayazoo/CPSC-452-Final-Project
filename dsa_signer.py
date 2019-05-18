3b857a63de8d7d1c72253366bfa48a82276c1418
Randy Le <97randy.le@gmail.com>
2019-05-18 13:54:20 -0700

    verify func in DSA

--- dsa_signer.py
+++ dsa_signer.py
@@ -5,20 +5,26 @@ from Crypto.Hash import SHA512
 def sign(message, key):
     hash = SHA512.new(message).hexdigest()
 
-    signature = key.sign(hash, key)
+    signature = key.sign(hash, 5)
 
-    signed_message = message + str(signature[0])
+    signed_message = message + "," + str(signature[0])
 
     return signed_message
 
-    
+
 
 # Verify the authenticity of the message
-#def verify():
-    
+def verify(signature, key, hash):
+    if key.verify(hash, signature):
+        print "Authentic"
+    else:
+        print "Incorrect"
+
 
 message = "Baboons rule! xDDDD"
 key = DSA.generate(1024)
 message = sign(message, key)
+contents = message.rsplit(',', 1)
+verifying = verify((long(contents[1])) key, contents[0])
 
 print(message)
