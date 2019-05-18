f631057c1c24cd8911d30335cb2146610069a7ec
Coleman Nugent <colemannugent@gmail.com>
2019-05-18 11:18:01 -0700

    server.py: Add OOP

--- server.py
+++ server.py
@@ -1,127 +1,177 @@
 import socket
 from _thread import *
 
-#set up server socket
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-ipaddr = '127.0.0.1'
-port = 64646
+class Client():
+	def __init__(self, conn, addr):
+		self.conn = conn
+		self.addr = addr
+		self.online = True
+		self.username = None
+		self.hash = None
+		self.authenticated = False
 
-server.bind((ipaddr, port))
-server.listen(100)
+	# Send a message to this client
+	def send(self, message):
+		# TODO: actually send the message
+		print("send called")
 
-#this list will contain all active clients
-clientList = []
+	# Close the connection to the client and mark them as offline
+	def close(self):
+		# TODO
+		print("close called")
 
-'''
-TODO: Fix this authentication function
-def user_auth(conn, addr):
-		
-	user_info = ""
-	status = ""
+	#TODO: Fix this authentication function
+	def auth(self):
 
-	while status != "success":
+		user_info = ""
+		status = ""
 
-		try:
-			msg = conn.recv(1024)
-			msg = msg.decode()
+		while status != "success":
 
-			credentials = msg.split()		
-
-			file = open("user-pass.txt", "r")
-
-			for info in file:
-				user_info = info.split()
-
-				if user_info[0] == credentials[0] and user_info[1] == credentials[1]:
-					status = "s"
-					print("Credentials confirmed.")
-					conn.send(status.encode())
-					file.close()
-					break
-
-			if(status != "success"):
-				print("Did you just print here?")
-				conn.send("f".encode())
-		
-		except:
-			continue
-'''
-
-#thread_client will be a thread that is running for each client to handled messages
-def thread_client(conn, addr):
-
-	#user_auth(conn, addr)
-
-	#welcome the client
-	welcomeMsg = "Welcome to Secure Chat!"
-	conn.send(welcomeMsg.encode())
-
-	#client main loop
-	while True:
-		try:
-			#try to recieve a message from the client
-			msg = conn.recv(1024)
-
-			#if a message has been recieved
-			if msg:
-				#print the message for debugging
-				#TODO: remove this print to because eventually all the traffic will be encrypted
-				print(addr[0] + ": " + msg.decode())
-
-				#send the message to all the connected users
-				broadcast(msg, conn)
-			else:
-				#this will handled when the server recieves a weird message. either a disconnect or error
-				#regardless we want to disconnect the client
-				remove(conn)
-
-		except:
-			continue
-
-#broadcast will send a message to all currently connected users except for the one passed as 'conn'
-#pass conn=null to broadcast to all
-def broadcast(msg, conn):
-	#loop through all currently connected clients and send the message
-	for client in clientList:
-
-		#if the client is not the one specified to be ignored
-		if client != conn:
 			try:
-				#send the message to the client
-				client.send(msg)
+				msg = self.conn.recv(1024)
+				msg = msg.decode()
+
+				credentials = msg.split()
+
+				file = open("user-pass.txt", "r")
+
+				for info in file:
+					user_info = info.split()
+
+					if user_info[0] == credentials[0] and user_info[1] == credentials[1]:
+						status = "s"
+						print("Credentials confirmed.")
+						self.authenticated = True
+						self.conn.send(status.encode())
+						file.close()
+						break
+
+				if status != "success":
+					print("Did you just print here?")
+					self.conn.send("f".encode())
 			except:
-				#if something goes wrong then disconnect the client
-				client.close()
-				remove(client)
+				continue
 
-#remove will remove a client from the client list
-def remove(conn):
-	if conn in clientList:
-		clientList.remove(conn)
+class ClientGroup():
 
-#the main function of the program
-def main():
-	print('Starting Server')
+	def __init__(self, members):
+		self.members = members
 
-	print('Waiting for clients...')
-	#our main server loop
-	while True:
+	# Send a message to all members of the group
+	def send(self, message):
+		# TODO
+		print("group send called")
 
-		#accept a client
-		conn, addr = server.accept()
+class Server():
+	def __init__(self, port, addr):
+		# Set up server socket
+		self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-		#add the connection onto the client list in order to make sure the list is always accurate
-		clientList.append(conn)
+		self.ipaddr = addr
+		self.port = port
 
-		print(addr[0] + ": connected!", addr)
+		# When the server is offline don't try to make any connections
+		self.online = True
 
-		#create a new thread for the newly connected client to handled all things related to that clientList
-		start_new_thread(thread_client, (conn, addr))
+		self.serverSocket.bind((self.ipaddr, self.port))
+		self.serverSocket.listen(100)
+
+		# Keep track of all the clients and the groups
+		self.clients = []
+		self.groups = []
 
 
-#start the server
-main()
-conn.close()
-server.close()
+	# TODO: convert to use the class
+	#thread_client will be a thread that is running for each client to handled messages
+	def thread_client(self, conn, addr):
+
+		#user_auth(conn, addr)
+
+		#welcome the client
+		welcomeMsg = "Welcome to Secure Chat!"
+		conn.send(welcomeMsg.encode())
+
+		#client main loop
+		while self.online:
+			try:
+				#try to recieve a message from the client
+				msg = conn.recv(1024)
+
+				#if a message has been recieved
+				if msg:
+					#print the message for debugging
+					#TODO: remove this print to because eventually all the traffic will be encrypted
+					print(addr[0] + ": " + msg.decode())
+
+					#send the message to all the connected users
+					broadcast(msg, conn)
+				else:
+					#this will handled when the server recieves a weird message. either a disconnect or error
+					#regardless we want to disconnect the client
+					remove(conn)
+
+			except:
+				continue
+
+	# TODO: do what main() used to do
+	def loop(self):
+		print('Waiting for clients...')
+		#our main server loop
+		while True:
+
+			#accept a client
+			conn, addr = self.serverSocket.accept()
+
+			#add the connection onto the client list in order to make sure the list is always accurate
+			# TODO: make a new client object and append it to our list of clients
+			self.clients.append(conn)
+
+			print(addr[0] + ": connected!", addr)
+
+			#create a new thread for the newly connected client to handled all things related to that clientList
+			start_new_thread(self.thread_client, (conn, addr))
+
+
+	# Send a message to all clients except for 'sender'
+	# Pass sender=null to broadcast to all
+	def broadcast(self, msg, sender):
+		#loop through all currently connected clients and send the message
+		for client in self.clients:
+
+			#if the client is not the one specified to be ignored
+			if client != sender:
+				try:
+					#send the message to the client
+					client.send(msg)
+				except:
+					#if something goes wrong then disconnect the client
+					client.close()
+					self.remove(client)
+
+	# Remove a client and close it's connection
+	def remove(self, client):
+		if client in self.clients:
+			client.close()
+			self.clients.remove(client)
+
+	# Close all client connections and close the server socket
+	def close(self):
+		print("server close called")
+
+		# Close the connections to all clients
+		for client in self.clients:
+			client.close()
+			self.clients.remove(client)
+
+		# Close the server socket
+		# TODO: is this how to close this socket?
+		self.serverSocket.close()
+
+
+print('Starting Server')
+# TODO: take these parameters from the commandline
+s = Server(64646, '127.0.0.1')
+s.loop()
